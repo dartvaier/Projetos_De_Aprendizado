95cd99aab137d26031cb0abb08ae650ec8b9e90a
dartvaier <gabrielsaprisqui@gmail.com>
2022-11-16 20:18:52 -0300

    consertando o JS
    Alterando o nome das classes escritas no JS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const botaoMenu = document.querySelector('.cabecalho-menu');
+const botaoMenu = document.querySelector('.cabecalho__menu');
 const menu = document.querySelector('.menu-lateral')
 
 botaoMenu.addEventListener('click', () =>{
-    menu.classList.toggle('menu-lateral-ativo')
+    menu.classList.toggle('menu-lateral--ativo')
 })
